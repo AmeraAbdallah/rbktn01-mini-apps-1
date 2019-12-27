ddb90e2e523aa907ba1fbbc36703e9950d766f89
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-27 19:43:56 +0100

    has global variables and start btn on click handler

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,16 @@
+let board;
+let playerx;
+let playero;
+let game;
 
+document.getElementById('start-btn').addEventListener('click', function(){
 
+  let name1 = document.getElementById('player-one-inpt').value;
+  let name2 = document.getElementById('player-two-inpt').value;
+  if(name1 === '' || name2 === ''){
+    return alert('please fill your names');
+  }
+  startNewGame(name1, name2);
+  displayPlayersInfo(playerx, playero);
+
+});
