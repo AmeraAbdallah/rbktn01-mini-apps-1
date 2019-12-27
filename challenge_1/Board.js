57d945fcbee4c89dc24ae510445bafdf7ad6160f
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-27 12:27:16 +0100

    remove es6 class because its not in basic requirement

--- challenge_1/Board.js
+++ challenge_1/Board.js
@@ -1,22 +1,19 @@
-class Board{
+function Board() {
 
-  costructor(){
-  }
+}
 
-  createBoard = () => {
-    let board = document.createElement('table');
-    for(let i = 0; i < 3; i ++){
-      let row = document.createElement('tr');
-      for(let j = 0; j < 3; j++){
-        let col = document.createElement('td');
-        let txtNode = document.createTextNode('0');
-        col.appendChild(txtNode);
-        col.setAttribute('data-index', `${i}${j}`);
-        row.appendChild(col);
-      }
-      board.appendChild(row);
+Board.prototype.createBoard = () => {
+  let board = document.createElement('table');
+  for(let i = 0; i < 3; i ++){
+    let row = document.createElement('tr');
+    for(let j = 0; j < 3; j++){
+      let col = document.createElement('td');
+      let txtNode = document.createTextNode('0');
+      col.appendChild(txtNode);
+      col.setAttribute('data-index', `${i}${j}`);
+      row.appendChild(col);
     }
-    document.getElementById('board').appendChild(board);
+    board.appendChild(row);
   }
-
+  document.getElementById('board').appendChild(board);
 }
