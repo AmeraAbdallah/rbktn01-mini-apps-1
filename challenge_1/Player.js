aa83939408b2d652e298592645b4764b6bf594f3
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-27 12:19:05 +0100

    add player logic

--- challenge_1/Player.js
+++ challenge_1/Player.js
@@ -1,11 +1,10 @@
-class Player{
-  constructor(name, element){
-    this.name = name;
-    this.element = element;
-    this.points = 0;
-  }
 
-  addPoint = () => {
-    return ++this.points;
-  }
+function Player(name, element) {
+  this.name = name;
+  this.element = element;
+  this.points = 0;
+}
+
+Player.prototype.addPoint = () => {
+  return ++this.points;
 }
