bdda5de9e644df1e35844f15cdcbd18be68c87e6
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-27 14:50:29 +0100

    fix game and add switch turn and play functions

--- challenge_1/Game.js
+++ challenge_1/Game.js
@@ -6,6 +6,15 @@ function Game(playerX, playerY){
   this.lastWinner = null;
 }
 
-Game.prototype.setLastWinner = (player) => {
+Game.prototype.setLastWinner = function(player){
   this.lastWinner = payer;
+}
+
+Game.prototype.switchTurn = function(){
+  this.turn = (this.turn === this.playerX) ? this.playerY : this.playerX;
+}
+
+Game.prototype.play = function(index){
+  document.getElementById(index).textContent = this.turn.element;
+  this.switchTurn();
 }
